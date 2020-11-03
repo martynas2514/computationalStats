90ac60d94960d405cb9c72bb8c056e5be0479f12
zahrajalilpour292 <zahja096@student.liu.se>
2020-11-03 21:21:01 +0100

    question1 R code  and rmd file

---
+++ lab1.R
@@ -0,0 +1,20 @@
+x1<- 1/3 
+x2<-1/4
+if ( x1-x2==1/12) {
+  print("Subtraction is correct")
+} else{
+  print("Subtraction is wrong")
+}
+
+x1 <- 1
+x2<- 1/2
+if (x1 - x2 == 1/2){
+  print("Subtraction is correct")
+}else{
+  print("Subtraction is wrong")
+}
+
+# In the first snippet code, the result is "Subtraction is wrong", but the second snippet code the result is "Subtraction is correct". 
+# The only numbers that represent exactly in r , are integers and fractions by power of 2 dominator. Hence all other
+# numbers are rounded to 53 binary digits accuracy.When ever floating point operations are done , we should assume that there wil be  numeric error. 1/3 and 1/12 are repeating decimals that are rounded in R.
+# You can use the all.equal function instead of == to test equality of floating point numbers.
