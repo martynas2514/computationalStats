d26cdb0ae1ad91c4fb26236b2965c09f0af2b601
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-12-02 14:49:06 +0100

    waf

--- lab5/Assignment2.R
+++ lab5/Assignment2.R
@@ -1,9 +1,44 @@
 library("boot")
-stat1<-function(data, vn){
-data<- as.data.frame(data[vn, ])
-res<-lm(Response~Predictor, data)
-res$coefficients[2]
+
+
+## 1 Plot the histogram of Price. Does it remind any conventional distribution? Compute the mean price.
+prices = read.csv("C:/Users/alejo/Documents/GitHub_Repos/computationalStats/lab5/prices1.csv", sep = ";")  
+
+hist(prices$Price, breaks = 15)
+
+## Chi Square?
+
+## Mean 
+mean_price <- mean(prices$Price)
+
+## 2 Estimate the distribution of the mean price of the house using bootstrap
+
+
+mean_stat <- function(data, vn){
+  data<- as.data.frame(data[vn, ])
+  data_mean <- as.numeric(colMeans(data['Price']))
+  data_mean
 }
+
+ans <- boot(data = prices, statistic = mean_stat, R=2000)
+ans
+
+plot(ans)
+
+# CI percentil
+boot.ci(ans, index = 1, type=c('perc'))
+
+# CI bca
+
+boot.ci(ans, index =1, type=c('bca'))
+
+# CI normal
+
+boot.ci(ans, index =1, type=c('norm'))
+
+## Bias-correction
+
+T_bias_correction <- function(data, vn){}
 
 x <- rnorm(100) ; data<-cbind(Predictor=x, Response= 3+2*x+rnorm(length(x), sd=0.5))
 res <- boot(data, stat1, R=1000)
